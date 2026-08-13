2092d36198b52a1bc0085e6d01d3a9b8a825d205
D-DID <nochung404@gmail.com>
2026-08-13 16:47:44 +0900

    fix: 코드 수정

--- src/main/java/com/meta/jpacore/entity/Memo.java
+++ src/main/java/com/meta/jpacore/entity/Memo.java
@@ -1,15 +1,13 @@
 package com.meta.jpacore.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name="memo")
 public class Memo {
     @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY) //잠시해제
     private  Long id;
 
     //nullable =null 허용 여부
